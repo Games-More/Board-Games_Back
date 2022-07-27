c141c4901ac11d088a62b297bd1800cb772cd704
GuzmanMartinez <67272145+GuzmanMartinez@users.noreply.github.com>
2022-07-27 18:46:36 +0100

    Limpieza de código

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// const express = require('express')
-// const a = express()
-// const port = process.env.PORT || 3000
-// a.get('/', (req, res) => {
-//     res.type('text/plain')
-//     res.send('Server Expresso ☕')
-// })
-// a.get('/about', (req, res) => {
-//     res.type('text/plain')
-//     res.send('Server Expresso ☕ About')
-// })
-// a.use((req, res) => {
-//     res.type('text/plain')
-//     res.status(404)
-//     res.send('404 Not found ☕_☕')
-// })
-// a.listen(port, () => console.log(`Expresso ☕ is on Port ${port} Ctrl + C to Stop `))
-// Requerir express y crear una instancia de ello
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = 4000;
 
-// en la solicitud de root (localhost:3000/)
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
     res.send('<b> Mi </b> primer servidor http express');
 });
 
@@ -34,7 +17,7 @@ app.use(function(req, res, next) {
     res.status(404).send("Lo siento, esa ruta no existe. Que tengas un buen día :)");
 });
 
-// inicie el servidor en el puerto 3000!
-app.listen(3000, function () {
-    console.log('Aplicación de ejemplo escuchando en el puerto 3000.');
+// inicie el servidor en el puerto 4000!
+app.listen(port, () => {
+    console.log(`Aplicación de ejemplo escuchando en el puerto ${port}.`);
 });
