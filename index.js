a2abc7727027ddc3df1547ec1442fe841c74c1cb
Guille <guifers23@gmail.com>
2022-07-27 19:31:05 +0200

    Servidor express funcionando

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-import * as express from 'express';
-
-
+// const express = require('express')
+// const a = express()
+// const port = process.env.PORT || 3000
+// a.get('/', (req, res) => {
+//     res.type('text/plain')
+//     res.send('Server Expresso ☕')
+// })
+// a.get('/about', (req, res) => {
+//     res.type('text/plain')
+//     res.send('Server Expresso ☕ About')
+// })
+// a.use((req, res) => {
+//     res.type('text/plain')
+//     res.status(404)
+//     res.send('404 Not found ☕_☕')
+// })
+// a.listen(port, () => console.log(`Expresso ☕ is on Port ${port} Ctrl + C to Stop `))
+// Requerir express y crear una instancia de ello
+var express = require('express');
 var app = express();
 
-// Create an http server with Node's HTTP module. 
-// Pass it the Express application, and listen on port 8080. 
-var server = require('http').createServer(app).listen(8080);
+// en la solicitud de root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send('<b> Mi </b> primer servidor http express');
+});
 
-// Instantiate Socket.IO hand have it listen on the Express/HTTP server
-var io = require('socket.io').listen(server);
+// En localhost:3000/welcome
+app.get('/welcome', function (req, res) {
+    res.send('<b>Holi</b> Bienvenido a mi servidor http hecho con express');
+});
 
-io.sockets.on('connection', function (socket) {
-  console.log('client connected');
-  console.log(io, socket);
+// Cambiar el mensaje 404 modificando el middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Lo siento, esa ruta no existe. Que tengas un buen día :)");
+});
+
+// inicie el servidor en el puerto 3000!
+app.listen(3000, function () {
+    console.log('Aplicación de ejemplo escuchando en el puerto 3000.');
 });
